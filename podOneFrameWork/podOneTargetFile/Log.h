8905e74556129ef1d6a02b9d6d94bb34d94986bf
tianhongyuan <944121333@qq.com>
2020-12-24 15:09:12 +0800

    first commit

---
+++ podOneFrameWork/podOneTargetFile/Log.h
@@ -0,0 +1,18 @@
+//
+//  Log.h
+//  podOneFrameWork
+//
+//  Created by changxu on 2020/12/24.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Log : NSObject
+
+- (void)log;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ podOneFrameWork/podOneTargetFile/Log.m
@@ -0,0 +1,14 @@
+//
+//  Log.m
+//  podOneFrameWork
+//
+//  Created by changxu on 2020/12/24.
+//
+
+#import "Log.h"
+
+@implementation Log
+- (void)log {
+    NSLog(@"test log stream");
+}
+@end
